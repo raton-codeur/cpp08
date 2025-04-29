e2424f6f3ea3fe69a963c7e3cb06b8115be68ccd
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-04-29 14:03:39 +0200

    blabla

--- repo/ex02/inc/MutantStack.hpp
+++ repo/ex02/inc/MutantStack.hpp
@@ -2,6 +2,7 @@
 #define MUTANTSTACK_HPP
 #include <iostream>
 #include <stack>
+#include <list>
 
 template <typename T>
 class MutantStack : public std::stack<T>
@@ -11,6 +12,20 @@ class MutantStack : public std::stack<T>
 						MutantStack(const MutantStack& other);
 		MutantStack<T>&	operator=(const MutantStack& other);
 						~MutantStack();
+		
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+
+		iterator				begin();
+		iterator				end();
+		const_iterator			begin() const;
+		const_iterator			end() const;
+		reverse_iterator		rbegin();
+		reverse_iterator		rend();
+		const_reverse_iterator	rbegin() const;
+		const_reverse_iterator	rend() const;
 };
 #include "MutantStack.tpp"
 
--- repo/ex02/src/main.cpp
+++ repo/ex02/src/main.cpp
@@ -1,13 +1,53 @@
 #include "MutantStack.hpp"
 
+template <typename Iterator>
+void print(Iterator beg, Iterator end)
+{
+	if (beg != end)
+		std::cout << *beg++;
+	while (beg != end)
+		std::cout << ", " << *beg++;
+	std::cout << std::endl;
+}
+
 int main()
 {
-	MutantStack<int> mstack;
+	{
+		std::cout << "--- 1 ---" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		mstack.push(4);
 
-	mstack.push(5);
-	mstack.push(17);
+		std::cout << "stack (→) : ";
+		print(mstack.begin(), mstack.end());
+		std::cout << "stack (←) : ";
+		print(mstack.rbegin(), mstack.rend());
+		std::cout << "top : " << mstack.top() << std::endl;
+		std::cout << "size : " << mstack.size() << std::endl;
+		
+		mstack.pop();
+		std::cout << "* pop *" << std::endl;
+		std::cout << "stack (→) : ";
+		print(mstack.begin(), mstack.end());
+		
+		MutantStack<int>::iterator it = mstack.begin();
+		++it;
+		--it;
+		
+		std::cout << "--- 2 ---" << std::endl;
+		const MutantStack<int> s(mstack);
+		std::cout << "stack (→) : ";
+		print(s.begin(), s.end());
+	}
+	{
+		std::cout << "--- 3 ---" << std::endl;
+		std::list<int> lst;
+		lst.push_back(5);
+		lst.push_back(17);
+		lst.push_back(4);
 
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.top() << std::endl;
+		std::cout << "list : ";
+		print(lst.begin(), lst.end());
+	}
 }
